ee12bbf267055efbd2a6a779d792cb3034139dac
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-04-23 08:48:21 +0200

    removed import

--- backend/src/routes/auditorium.routes.js
+++ backend/src/routes/auditorium.routes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const controller = require('../controllers/auditorium.controllers');
-//const auth = require('../middlewares/userAuthentication.middlewares');
 
 const router = express.Router();
 
 
-router.get('/audi', controller.getAllAuditoriums)
-router.get('/audi/:id', controller.getAuditoriumById);
-router.delete('/audi/delete/:id',  controller.deleteAuditorium);
-router.patch('/audi/update/:id', controller.updateAuditorium);
-router.post('/audi/create', controller.createAuditorium);
+router.get('/auditorium', controller.getAllAuditoriums)
+router.get('/auditorium/:id', controller.getAuditoriumById);
+router.delete('/auditorium/:id',  controller.deleteAuditorium);
+router.patch('/auditorium/:id', controller.updateAuditorium);
+router.post('/auditorium', controller.createAuditorium);
 
 
 module.exports = router;
