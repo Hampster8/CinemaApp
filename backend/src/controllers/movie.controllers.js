e8fd5991a4ef8fce918492d69ba30fa37a330e6c
Jens Vighagen <jvighagen@gmail.com>
2022-04-23 13:24:06 +0200

    createMovie is created and a fix in getMovies

--- backend/src/controllers/movie.controllers.js
+++ backend/src/controllers/movie.controllers.js
@@ -4,8 +4,8 @@ const movieController = require('../models/movie.models');
 const getAllMovies = async (req, res) => {
 
     try {
-        const getMovies = await movieController.findAll();
-        return res.json(getMovies);
+        const getMovies = await movieController.find();
+        res.json(getMovies);
 
     } catch (err) {
         return res.status(401).json({error: 'Something went wrong, try again!'});
@@ -13,7 +13,27 @@ const getAllMovies = async (req, res) => {
     }
 }
 
+const createMovie = async (req, res) => {
+
+    const post = new movieController ({
+
+        id: req.params.id,
+        title: req.body.title,
+        category: req.body.category,
+        playTime: req.body.playTime
+    })
+
+    try {
+        const saveMovie = await post.save();
+        res.json(saveMovie);
+    } catch (err) {
+        res.status(400);
+    }
+
+}
+
 
 module.exports = {
-    getAllMovies
+    getAllMovies,
+    createMovie
 };
--- backend/src/routes/movie.routes.js
+++ backend/src/routes/movie.routes.js
@@ -4,6 +4,9 @@ const controller = require('../controllers/movie.controllers');
 const router = express.Router();
 
 // Define the routes for movies
+router.post('/', controller.createMovie);
+
+
 router.get('/', controller.getAllMovies);
 
 
