85dce0141c780ba5e8513f0630036e603101c6ce
dtrix90 <dominique_90_@hotmail.com>
2022-04-27 13:31:41 +0200

    header component

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthenticationProvider } from './src/contexts/authentication.contexts';
 import Router from './src/router';
+import Header from './src/components/header.components';
 
 
 
@@ -11,6 +12,7 @@ const App = () => {
     return (
         <AuthenticationProvider>
             <Router />
+            <Header />
         </AuthenticationProvider>
     );
 }
