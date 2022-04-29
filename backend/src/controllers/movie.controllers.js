02512929294010915d3203361a6aabbd0dc4a9e9
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-04-29 14:40:54 +0200

    get specificm movie from id

--- backend/src/controllers/movie.controllers.js
+++ backend/src/controllers/movie.controllers.js
@@ -11,11 +11,20 @@ const getAllMovies = async (_req, res) => {
 
 const createMovie = async (req, res) => {
     const movie = new Movie(req.body);
-    console.log(movie);
     movie.save(e => {
         if (e) return res.status(422).json({error: 'requries imdbID and playingNow'});
         return res.sendStatus(200);
     });
+}
+
+const getOneMovie = async (req, res) => {
+    Movie.find({imdbID: req.params.imdbID}, (e, movies) => {
+        if (e) return res.status(422).json({error: 'Not valid id'});
+        console.log(movies);
+        omdbapi(movies).then((data) => {
+            res.status(200).json(data);
+        });
+    })
 }
 
 const deleteMovie = async (req, res) => {
@@ -36,6 +45,7 @@ const omdbapi = async (movieRecords) => {
 }
 
 module.exports = {
+    getOneMovie,
     getAllMovies,
     createMovie,
     deleteMovie
--- backend/src/routes/movie.routes.js
+++ backend/src/routes/movie.routes.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/movie.controllers');
 const router = express.Router();
 
 router.get('/', controller.getAllMovies);
+router.get('/:imdbID', controller.getOneMovie);
 router.post('/', controller.createMovie);
 router.delete('/:imdbID', controller.deleteMovie);
 
