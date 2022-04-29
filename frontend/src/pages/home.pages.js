9e1b0fe6ac779cc751c171bf7096a7fede49839b
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-04-29 15:01:50 +0200

    homescreen  now retrives all the movies

--- frontend/src/pages/home.pages.js
+++ frontend/src/pages/home.pages.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const HomePage = () => {
+
+    const [moviesData, SetMoviesData] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/movies')
+        .then(res => res.json())
+        .then(data => SetMoviesData(data))
+    }, []);
+
+    console.log(moviesData);
+
     return (
         <div>
-            <p>HOME</p>
+            {moviesData.map((movie, key) => {
+                return (
+                    <div key={key}>
+                        <img src={movie.Poster} />
+                        <p>{movie.Title}</p>
+                    </div>)
+            })}
         </div>
     );
 }
