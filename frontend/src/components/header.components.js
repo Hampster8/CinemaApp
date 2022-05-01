c41e6d843836ba2f6edb439c38b394c2d8423f0b
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-05-01 19:06:00 +0200

    Fixed small css changes

--- frontend/src/components/header.components.js
+++ frontend/src/components/header.components.js
@@ -3,49 +3,45 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='navbar' style={navBar}>
-            <h1 style={cinemaStyle}>Cinema</h1>
-            <div className='nav-items' style={navItems}>
-                <Link to="/allmovies" style={navA}>All movies</Link>
-                <Link to="/aboutus" style={navA}>About us</Link>
-                <Link to="/mytickets" style={navA}>My tickets</Link>
+        <div className='navbar' style={style.navBar}>
+            <h1 style={style.cinemaStyle}>Cinema</h1>
+            <div className='nav-items' style={style.navItems}>
+                <Link to="/allmovies" style={style.navA}>All movies</Link>
+                <Link to="/aboutus" style={style.navA}>About us</Link>
+                <Link to="/mytickets" style={style.navA}>My tickets</Link>
             </div>
         </div>
-
     );
 }
 
-const cinemaStyle = {
-    fontFamily: 'Pacifico',
-    fontStyle: 'normal',
-    fontWeight: '400',
-    fontSize: '50px',
-    color: '#009556',
-    margin: '15px'
-
-};
-
-const navBar = {
-  
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItemts: 'center',
-    background: '#141414',
-};
-
-const navItems = {
-    fontFamily: 'Nunito',
-    fontStyle: 'normal',
-    fontWeight: '700',
-    paddingTop: '60px'
-};
-
-const navA = {
-    textDecoration: 'none',
-    color: '#FFFFFF',
-    opacity: '0.2',
-    margin: '15px'
-};
-
+const style = {
+    cinemaStyle: {
+        fontFamily: 'Pacifico',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: '50px',
+        color: '#009556',
+        margin: '15px'
+    },
+    navBar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItemts: 'center',
+        background: '#141414',
+    },
+    
+    navItems: {
+        fontFamily: 'Nunito',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        paddingTop: '60px'
+    },
+    navA: {
+        textDecoration: 'none',
+        color: '#FFFFFF',
+        opacity: '0.2',
+        margin: '15px'
+    }
+}
 
 export default Header;
