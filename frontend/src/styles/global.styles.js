39395a18278483858fe51ceda98c0eb65c72fc3d
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-05-01 19:24:38 +0200

    r undefined style

--- frontend/src/styles/global.styles.js
+++ frontend/src/styles/global.styles.js
@@ -1,24 +1,3 @@
 export const primaryColor = '#009556';
 export const secondaryColor = '#1F1F1F';
 export const tertiaryColor = '#141414';
-
-export const rootStyle = {
-    //backgroundColor: tertiaryColor,
-    width: '100%',
-    height: '100%',
-    margin: 0,
-    padding: 0,
-    position: 'absolute'
-}
-
-export const button = {
-    backgroundColor: 'green'
-}
-
-// export all styles
-export const styles = {
-    button,
-    primaryColor,
-    secondaryColor,
-    tertiaryColor
-}
