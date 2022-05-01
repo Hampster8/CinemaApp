c268902d640acfea5a270eba8c23e5bc78e82b52
Jens Vighagen <jvighagen@gmail.com>
2022-05-01 19:54:41 +0200

    added movieStyle to the movieInfo.comp

--- frontend/src/components/movieInfo.components.js
+++ frontend/src/components/movieInfo.components.js
@@ -3,15 +3,27 @@ import React from 'react';
 const MovieInfo = ({ imageUrl, infoProps, openInfoCallback }) => {
 
     return (
-        <div>
+        <div style={movieStyle.container}>
             <img src={imageUrl} />
-            <p>{infoProps}</p>
-            <button onClick={() => openInfoCallback}>More Information</button>
+            <p style={movieStyle.infoText}>{infoProps}</p>
+            <button style={movieStyle.styleButton} onClick={() => openInfoCallback}>More Information</button>
         </div>
     );
 }
 
-const style = {
+const movieStyle = {
 
+    container: {
+        // style for div
+    },
+
+    infoText: {
+        // face the text
+    },
+
+    styleButton: {
+        backgroundColor: 'white', // change to the correct color when decided
+        border: 'none'
+    }
 }
 export default MovieInfo;
