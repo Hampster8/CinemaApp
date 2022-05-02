7b80723a401c940ce442920ba1e980b871cbbf02
Oscar Karlsson <oscar.karl00@hotmail.com>
2022-05-02 09:17:52 +0200

    get date examples for hampus

--- frontend/src/components/screeening.components.js
+++ frontend/src/components/screeening.components.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Screening = () => {
+
+    const [count, SetCount] = useState(0);
+
+    const IncreaseCount = (Increase) => {
+        SetCount(Increase ? count + 1 : count - 1)
+    }
+
+    const UpdateDate = () => {
+        const date = new Date();
+        date.setDate(date.getDate() + count );
+        return <p>{date.toString()}</p>
+    }
+
     return (
         <div>
-            <p>Screening</p>
+            <button onClick={() => IncreaseCount(true)} >Increase date</button>
+            <UpdateDate />
+            <button onClick={() => IncreaseCount(false)}>Decrease Date</button>
+
+            {/* Annars kan man göra så här */}
+
+            <input type='date' onChange={(e) => console.log(e.target.value)} />
+
         </div>
     );
 }
