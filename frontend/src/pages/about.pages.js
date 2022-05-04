5e19c83ce4c734e7d89fd94adca55fa4f0497688
Hampus Andersson <70724079+Hampster8@users.noreply.github.com>
2022-05-04 13:14:14 +0200

    template aboutus

--- frontend/src/pages/about.pages.js
+++ frontend/src/pages/about.pages.js
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import { GitHubSVG } from '../styles/global.styles';
 
 const AboutPage = () => {
     return (
-        <div>
-            <p>About us page</p>
-        </div>
+            <div style={styles.text}>
+                <h1>Hi!</h1>
+                <p>We are five students at Högskolan Kristianstad who decided upon creating a Cinema App for educational purposes. <br/><br/>
+                
+                <b>Created by:</b><br/><br/>
+                <a style={styles.text} href="https://github.com/Waveya" target="_blank">Linnéa Svensson</a><br/>
+                <a style={styles.text} href="https://github.com/dtrix90" target="_blank">Dominique Ghaffari</a><br/>
+                <a style={styles.text} href="https://github.com/jvighagen" target="_blank">Jens Vighagen</a><br/>
+                <a style={styles.text} href="https://github.com/OsvarK" target="_blank">Oscar Karlsson</a><br/>
+                <a style={styles.text} href="https://github.com/Hampster8" target="_blank">Hampus Andersson</a><br/>
+                </p>
+                <b>Link to Project:</b>
+                <div style={{ height: 30, margin: 0, padding: 0}}><a href="https://github.com/Hampster8/CinemaApp" target="_blank"><GitHubSVG /></a></div>
+            </div>
     );
+}
+
+const styles = {
+    text: {
+        color: 'white'
+    }
+
+
 }
 
 export default AboutPage;
